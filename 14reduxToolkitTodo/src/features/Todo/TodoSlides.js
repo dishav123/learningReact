91ea7889e2b87ab16b3f2792edb49e713eac4ed1
Dishav <dshpshrestha@gmail.com>
2025-09-20 21:54:23 +0545

    Redux concept

---
+++ 14reduxToolkitTodo/src/features/Todo/TodoSlides.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello World!" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+
+    removeTodo: (state, action) => {
+      state.todos.filter((todo) => todo.id != action.payload);
+    },
+
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const updTodo = state.todos.find((todo) => todo.id == id);
+      if (updTodo) {
+        updTodo.text = text;
+      }
+    },
+  },
+});
+
+export const {addTodo,removeTodo}=todoSlice.actions;
+
+export default todoSlice.reducer;
